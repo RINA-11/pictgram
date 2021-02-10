9eea74da2a4f762a93044e9f8c50a89aaabdf64d
RINA <74470264+RINA-11@users.noreply.github.com>
2021-02-10 09:21:54 +0000

    Rails 04

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  validates:name,presence:true,length:{maximum:15}
+  validates:email,presence:true,format:{with:/\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/}
+  validates:password,length:{in:8..32},format:{with: /\A(?=.*?[a-z])[a-z\d]+\z/}
+  
+  has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  root "pages#index"
   get "pages/link"
   get "pages/help"
-  get 'pages/index'
-  root "pages#index"
+  get "pages/index"
   resources:users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
