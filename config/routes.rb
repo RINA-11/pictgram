c4728e726761288ffe9d7530dbf1af58c7335717
RINA <74470264+RINA-11@users.noreply.github.com>
2021-02-14 06:16:54 +0000

    Rails 05-2

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  root "pages#index"
-  get "pages/link"
-  get "pages/help"
-  get "pages/index"
   
-  resources:users
+  get "sessions/new"
+  
+  root "pages#index"
+  get "pages/help"
+  
+  get "pages/link"
+  get "pages/index"
   
   get    "/login",   to: "sessions#new"
   post   "/login",   to: "sessions#create"
   delete "/logout",  to: "sessions#destroy"
+  
+  resources:users
+  resources:topics
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
