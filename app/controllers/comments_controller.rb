2ac49af73a582b57f8b07c49ce119e33f9e1ff44
RINA <74470264+RINA-11@users.noreply.github.com>
2021-02-17 04:58:01 +0000

    Rails 07

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,7 +7,7 @@ class CommentsController < ApplicationController
   end
   
   def create
-    @comment=current_user.comments.new(comment_params)
+    @comment=Comment.new(comment_params)
     if @comment.save
       redirect_to topics_path,success:"コメントを投稿しました"
     else
